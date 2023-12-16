e3737544434c0049805dbd2bd274c404c52d0811
jdmedlock <jdmedlock@gmail.com>
2023-12-16 08:04:39 -0600

    Refactor: Cleanup code formatting, messages, and add additional columns to User table

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ import format from 'pg-format'
 import { faker } from '@faker-js/faker'
 import dotenv from 'dotenv'
 
-if (process.env.NODE_ENV !== 'production') {
-    dotenv.config()
-}
+dotenv.config()
 
 const sql = `
-drop table if exists users;
-create table users(id serial unique, firstname text, lastname text, phone text, address text);
+  drop table if exists users;
+  create table users(id serial unique, 
+    firstname text, 
+    lastname text, 
+    phone text, 
+    streetaddress text,
+    city text, 
+    zipcode text, 
+    countrycode text
+    );
 `
 
+// Create an array of numbers spanning a desired range
 const range = (start, end) => Array.from(
-    Array(Math.abs(end - start) + 1), 
-    (_, i) => start + i
-  )
+  Array(Math.abs(end - start) + 1), 
+  (_, i) => start + i
+)
 
-const f = async function () {
+// Define and load the test DB with data
+const createTestTable = async function () {
 
   const numRecords = process.env.DB_NO_ROWS === undefined 
     ? 10 
@@ -28,40 +36,42 @@ const f = async function () {
       ssl: { rejectUnauthorized: false }
   })
 
-  // connect
-  console.log('connecting')
+  // Connect to the PostgreSQL test DB
+  console.log('Connecting to database instance...')
   await client.connect()
 
-  // create table
-  console.log('creating table')
+  // Create a test table
+  console.log('Creating table...')
   await client.query(sql)
 
-  console.log('creating fake data')
-  const arr = range(0, numRecords).map(i => {
+  // Populate the test table with rows of data
+  console.log('Loading test data...')
+  const testData = range(0, numRecords-1).map(i => {
+    return {
+      firstname: `${ faker.person.firstName() }`,
+      lastname: `${ faker.person.lastName() }`,
+      phone: `${ faker.phone.number() }`,
+      streetaddress: `${ faker.location.streetAddress() }`,
+      city: `${ faker.location.city() }`,
+      zipcode: `${ faker.location.zipCode() }`, 
+      countrycode: `${ faker.location.countryCode('alpha-2') }`
+    }
+  }).map(x => [x.firstname, x.lastname, x.phone, x.streetaddress, x.city, x.zipcode, x.countrycode])
 
-      return {
-          firstname: `${faker.person.firstName()}`,
-          lastname: `${faker.person.lastName()}`,
-          phone: `${faker.phone.number()}`,
-          address: `${faker.location.streetAddress()}` + ', ' 
-              + `${faker.location.zipCode()}` + ' ' 
-              + `${faker.location.city()}` + ', ' 
-              + `${faker.location.county()}`
-      }
-  }).map(x => [x.firstname, x.lastname, x.phone, x.address])
-
-  console.log('arr: ', arr)
-
-  // perform insert
-  console.log('insert data')
-  const insertSql = format('insert into users( firstname, lastname, phone, address) values %L returning id', arr)
+  // Insert the test rows into the DB 
+  console.time('Inserting test data...')
+  const insertSql = format('insert into users( firstname, lastname, phone, streetaddress, city, zipcode, countrycode) values %L returning id', testData)
+  console.timeLog('Inserting test data...')
   const res = await client.query(insertSql)
-  console.log('inserted records: ', res.rowCount)
+  console.log(`Test DB created. Inserted ${ res.rowCount } rows`)
 }
 
-f().then(() => {
-  console.log('finished')
+createTestTable()
+.then(() => {
+  console.log('Sucessfully completed')
   process.exit(0)
-}).catch(error => {
+})
+.catch(error => {
   console.error(error)
+  process.exit(0)
 })
