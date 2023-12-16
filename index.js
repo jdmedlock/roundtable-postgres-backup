0afa28e25779bbd399570cfce998a9487a2cc395
Jim Medlock <jdmedlock@gmail.com>
2023-12-15 21:02:17 -0600

    Feature: Replace Lodash with native code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import PG from 'pg'
 import format from 'pg-format'
 import faker from 'faker'
 import dotenv from 'dotenv'
-import _ from 'lodash'
 
 if (process.env.NODE_ENV !== 'production') {
     dotenv.config()
@@ -13,46 +12,53 @@ drop table if exists counterparties;
 create table counterparties(id serial unique, name text, business text, phone text, address text);
 `
 
-var f = async function () {
+const range = (start, end) => Array.from(
+    Array(Math.abs(end - start) + 1), 
+    (_, i) => start + i
+  )
 
-    const numRecords = process.argv[2] === undefined ? 10 : Number(process.argv[2])
+const f = async function () {
 
-    const client = new PG.Client({
-        connectionString: process.env.DB_CONNECTION_STRING,
-        ssl: { rejectUnauthorized: false }
-    })
+  const numRecords = process.env.DB_NO_ROWS === undefined 
+    ? 10 
+    : Number(process.env.DB_NO_ROWS)
 
-    // connect
-    console.log('connecting')
-    await client.connect()
+  const client = new PG.Client({
+      connectionString: process.env.DB_CONNECTION_STRING,
+      ssl: { rejectUnauthorized: false }
+  })
 
-    // create table
-    console.log('creating table')
-    await client.query(sql)
+  // connect
+  console.log('connecting')
+  await client.connect()
 
-    console.log('creating fake data')
-    const arr = _.range(numRecords).map(i => {
-        const rc = faker.helpers.createCard()
+  // create table
+  console.log('creating table')
+  await client.query(sql)
 
-        return {
-            name: rc.company.name,
-            business: rc.company.bs,
-            phone: rc.phone,
-            address: rc.address.streetA + ', ' + rc.address.streetB + ', '
-                + rc.address.zipcode + ' ' + rc.address.city + ', ' + rc.address.county
-        }
-    }).map(x => [x.name, x.business, x.phone, x.address])
+  console.log('creating fake data')
+  const arr = range(0, numRecords).map(i => {
+      const rc = faker.helpers.createCard()
 
-    // perform insert
-    console.log('insert data')
-    const insertSql = format('insert into counterparties( name, business, phone, address) values %L returning id', arr)
-    const res = await client.query(insertSql)
-    console.log('inserted records: ', res.rowCount)
+      return {
+          name: rc.company.name,
+          business: rc.company.bs,
+          phone: rc.phone,
+          address: rc.address.streetA + ', ' + rc.address.streetB + ', '
+              + rc.address.zipcode + ' ' + rc.address.city + ', ' + rc.address.county
+      }
+  }).map(x => [x.name, x.business, x.phone, x.address])
+
+  // perform insert
+  console.log('insert data')
+  const insertSql = format('insert into counterparties( name, business, phone, address) values %L returning id', arr)
+  const res = await client.query(insertSql)
+  console.log('inserted records: ', res.rowCount)
 }
 
 f().then(() => {
-    console.log('finished')
-    process.exit(0)
+  console.log('finished')
+  process.exit(0)
 }).catch(error => {
-    console.error(error)
+  console.error(error)
 })
